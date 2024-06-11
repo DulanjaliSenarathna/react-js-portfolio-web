9003dcda0436f273cfdada6630b427b557dc8a89
Dulanjali Senarathna <dishanisenarathna96@gmail.com>
2024-06-11 11:11:37 +0530

    Blog section done

--- src/components/blog/Blog.jsx
+++ src/components/blog/Blog.jsx
@@ -1,8 +1,65 @@
 import React from 'react'
+import { FaArrowRight } from 'react-icons/fa';
+import Blog1 from '../../assets/blog1.jpg'
+import Blog2 from '../../assets/blog2.jpg'
+import Blog3 from '../../assets/blog3.jpg'
+import './blog.css'
 
 const Blog = () => {
   return (
-    <div>Blog</div>
+    <section className="blog section" id="blog">
+        <h2 className="section__title text-cs">Latest Blog</h2>
+        <p className="section__subtitle">
+            My <span>Articles and Advice</span>
+        </p>
+
+        <div className="blog__container container grid">
+            <div className="blog__item card card-two">
+                <span className="blog__date text-cs">OCTOBER 31, 2023</span>
+                <h3 className="blog__title">The main thing for the designer</h3>
+                <p className="blog__description">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Unde ad placeat dolor maiores nesciunt expedita, dolore officiis veniam quos praesentium?</p>
+
+                <a href="" className="link">
+                    Read more
+                    <FaArrowRight className='link__icon'></FaArrowRight>
+
+                </a>
+
+                <img src={Blog1} alt="" className="blog__img" />
+
+            </div>
+
+            <div className="blog__item card card-two">
+                <span className="blog__date text-cs">OCTOBER 31, 2023</span>
+                <h3 className="blog__title">The main thing for the designer</h3>
+                <p className="blog__description">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Unde ad placeat dolor maiores nesciunt expedita, dolore officiis veniam quos praesentium?</p>
+
+                <a href="" className="link">
+                    Read more
+                    <FaArrowRight className='link__icon'></FaArrowRight>
+
+                </a>
+
+                <img src={Blog2} alt="" className="blog__img" />
+
+            </div>
+
+            <div className="blog__item card card-two">
+                <span className="blog__date text-cs">OCTOBER 31, 2023</span>
+                <h3 className="blog__title">The main thing for the designer</h3>
+                <p className="blog__description">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Unde ad placeat dolor maiores nesciunt expedita, dolore officiis veniam quos praesentium?</p>
+
+                <a href="" className="link">
+                    Read more
+                    <FaArrowRight className='link__icon'></FaArrowRight>
+
+                </a>
+
+                <img src={Blog3} alt="" className="blog__img" />
+
+            </div>
+        </div>
+    </section>
   )
 }
 
